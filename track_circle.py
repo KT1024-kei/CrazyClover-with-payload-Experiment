4f6788425fc94bb0218325097e8e7901279e388e
Kato-ibukiexp_laptop <kt0120katu@gmail.com>
2022-12-12 15:32:32 +0900

    modify for Experiment

--- track_circle.py
+++ track_circle.py
@@ -2,80 +2,105 @@
 # coding: UTF-8
 
 import sys
-sys.path.append('../')
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 
-from demo_Experiment.Env_experiment import Env_Experiment
-from Controller.Controllers import Controllers
-from Drone.Drone_with_Load_model import Drone_with_cable_suspended
+# from ros_ws.src.crazyswarm.scripts.pycrazyswarm.crazyswarm import Crazyswarm
+from pycrazyswarm import *
+
+from Env_experiment import Env_Experiment
+from Exp_Controller.Controllers import Controllers
 
 def Experiment(Texp, Tsam, num_drone):
+
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    cf = swarm.allcfs.crazyflies
 
     Env = Env_Experiment(Texp, Tsam, 0)
     Drone_env = [0]*num_drone
     Drone_ctrl = [0]*num_drone
-    Drone_log = [0]*num_drone
-    # swarm = Crazyswarm()
-    # timeHelper = swarm.timeHelper
-    # cf = swarm.allcfs.crazyflies
-
+    Tsam_lock = Tsam
 
     zero = np.zeros(3)
     for i in range(num_drone):
         Drone_env[i] = Env_Experiment(Texp, Tsam, i)
-        Drone_ctrl[i] = Controllers(Tsam, "QCSL")
-        Drone_env[i].track_hover_payload(Drone_ctrl[i], True)
+        Drone_ctrl[i] = Controllers(Tsam, "mellinger")
 
-    cf = [0]*num_drone
-    for i in range(num_drone):
-      cf[i] = Drone_with_cable_suspended(Tsam)
-      Drone_env[i].init_state(cf[i], P=np.array([0.1, 0.0, 0.0]))
-      Drone_env[i].init_plot(None)
+    for i in range(num_drone):          # ホバリング
+        Drone_env[i].quad_takeoff(Drone_ctrl[i])
+        timeHelper.sleep(2)             # 5秒静止
 
-    circle_flag = True
+    track_flag = True
+    stop_flag = True
+    land_flag = True
+
+    takeoff_time = 10
+    Texp = Texp + takeoff_time
+    stop_time = Texp + 5
+    land_time = stop_time + 15
+    
+    Ts = timeHelper.time()
+    Te = -Tsam
     t = 0
-    cnt = 0
+
     while True:
 
         for i in range(num_drone):
-            Drone_env[i].take_log(t, Drone_ctrl[i])
-
-        for i in range(num_drone):
-            Drone_env[i].update_state(cf[i])
-
-        if t > 3.0:
+            Env.set_clock(t)
+            Drone_env[i].set_clock(t)
+            Drone_env[i].take_log(Drone_ctrl[i])    #　状態と入力を記録
+        
+        if takeoff_time < t < Texp:
             for i in range(num_drone):
-                if circle_flag:
-                    Drone_ctrl[i].switch_controller("QCSL")
-                    Drone_env[i].track_circle(Drone_ctrl[i])
-                    circle_flag = False
-        
-        for i in range(num_drone):
-            Drone_ctrl[i].set_state(Drone_env[i].P, Drone_env[i].V, Drone_env[i].R, Drone_env[i].Euler, Drone_env[i].L, Drone_env[i].dL, Drone_env[i].q, Drone_env[i].dq)
+                if track_flag:
+                    Drone_env[i].quad_tack_circle(Drone_ctrl[i])
+                    track_flag = False
+
+        if Texp < t < stop_time:
+            for i in range(num_drone):
+                if stop_flag:
+                    Drone_env[i].quad_stop_track(Drone_ctrl[i])
+                    stop_flag = False
+
+        if t > stop_time:                      # 着陸
+            for i in range(num_drone):
+                if land_flag:
+                    Drone_env[i].quad_land(Drone_ctrl[i])
+                    land_flag = False
+
+        for i in range(num_drone):      # コントローラに状態を渡して入力加速度と角速度を計算
+            Drone_ctrl[i].set_state(Drone_env[i].P, Drone_env[i].Vfiltered, Drone_env[i].R, Drone_env[i].Euler, Drone_env[i].Pl, Drone_env[i].Vl_filterd, Drone_env[i].q, Drone_env[i].dq_filtered)
             Drone_ctrl[i].get_output(t)
-
         
-        for i in range(num_drone):
-            cf[i].main(Drone_ctrl[i].input_acc, Drone_ctrl[i].input_Wb)
+        for i in range(num_drone):      # ドローンに入力を送信
+            input_acc = Drone_ctrl[i].input_acc
+            input_W = np.array([Drone_ctrl[i].input_Wb[0]*1, Drone_ctrl[i].input_Wb[1]*1, Drone_ctrl[i].input_Wb[2]])
+            cf[i].cmdFullState(zero, 
+                            zero, 
+                            np.array([0.0, 0.0, input_acc/100.0]), 
+                            0.0, 
+                            input_W)
+
+
+        t = timeHelper.time() - Ts      # ループ周期を一定に維持
+        Tsam = t - Te
+        Te = t
+        if Env.time_check(t - Te, land_time): break
 
 
         for i in range(num_drone):
-            if cnt/100 == 1:
-                Drone_env[i].update_plot(cf[i].world_frame)
-                plt.pause(Tsam*100)
-                cnt = 0
-            cnt += 1
+            Drone_env[i].set_dt(dt=Tsam)
+            if Drone_env[i].update_state(): # 状態を更新
+                cf[i].cmdFullState(zero, zero, zero, 0.0, zero)
+                exit()
 
-        if Env.time_check(t, Tsam, Texp): break
-        t += Tsam
-    
     for i in range(num_drone):
-        Drone_env[i].save_log()
+        cf[i].cmdFullState(zero, zero, zero, 0.0, zero)
+
 if __name__ == "__main__":
-  Experiment(50, 0.001, 1)
+    Experiment(20, 0.005, 1)
 
 
-
-
+# モータマップ4つめ修正
+# 制御周期を姿勢周期にする
